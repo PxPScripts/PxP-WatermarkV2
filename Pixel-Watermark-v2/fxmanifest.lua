b9e7848bdc9b3118fb2f46c64465155cbdbe968b
Pixel <100500099+PixelTheDeveloper@users.noreply.github.com>
2022-04-22 14:20:45 +0200

    Add files via upload

---
+++ Pixel-Watermark-v2/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Pixel#7068'
+description 'Pixel Watermark v2'
+version '2.0.0
+
+ui_page('html/index.html')
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
+files({
+    'html/index.html',
+    'html/css/style.css',
+    'html/css/bootstrap.min.css',
+    'html/js/script.js',
+    'html/js/bootstrap.min.js'
+})
